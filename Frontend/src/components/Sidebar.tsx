1f9c59b472970bcbc22a2a96bc2a57c594fbd250
Anshul Mankar <anshulmankar887@gmail.com>
2026-06-12 13:06:19 +0530

    Create Sidebar.tsx

---
+++ Frontend/src/components/Sidebar.tsx
@@ -0,0 +1,43 @@
+export default function Sidebar(){
+
+ return(
+
+ <aside className="
+  bg-slate-900
+  text-white
+  w-64
+  min-h-screen
+  p-5
+ ">
+
+  <h1 className="
+   text-xl
+   font-bold
+   mb-8
+  ">
+   SecureScan
+  </h1>
+
+  <ul className="space-y-4">
+
+   <li>
+    Dashboard
+   </li>
+
+   <li>
+    Projects
+   </li>
+
+   <li>
+    Scans
+   </li>
+
+   <li>
+    Reports
+   </li>
+
+  </ul>
+
+ </aside>
+ );
+}
