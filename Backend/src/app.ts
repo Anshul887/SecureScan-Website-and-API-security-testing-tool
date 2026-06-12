70ef66e97b268c8321c068d46940db3ea54daec6
Anshul Mankar <anshulmankar887@gmail.com>
2026-06-12 12:54:56 +0530

    Update app.ts

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes
+from "./routes/auth.routes";
+
+import userRoutes
+from "./routes/user.routes";
+
 const app = express();
 
 app.use(cors());
@@ -9,8 +15,19 @@ app.use(express.json());
 
 app.get("/", (_, res) => {
   res.json({
-    message: "SecureScan API Running"
+    message:
+      "SecureScan API"
   });
 });
+
+app.use(
+  "/api/auth",
+  authRoutes
+);
+
+app.use(
+  "/api/users",
+  userRoutes
+);
 
 export default app;
