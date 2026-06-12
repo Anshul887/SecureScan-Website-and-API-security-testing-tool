936d226abafd79b29ea4ce3c7508933809833a6e
Anshul Mankar <anshulmankar887@gmail.com>
2026-06-12 13:08:24 +0530

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,13 +1,53 @@
-function App() {
-  return (
-    <div>
-      <h1>SecureScan</h1>
+import {
+ BrowserRouter,
+ Routes,
+ Route
+}
+from "react-router-dom";
 
-      <p>
-        Website & API Security Testing Platform
-      </p>
-    </div>
-  );
+import Login
+from "./pages/Login";
+
+import Register
+from "./pages/Register";
+
+import Dashboard
+from "./pages/Dashboard";
+
+import ProtectedRoute
+from "./components/ProtectedRoute";
+
+function App(){
+
+ return(
+
+ <BrowserRouter>
+
+  <Routes>
+
+   <Route
+    path="/"
+    element={<Login/>}
+   />
+
+   <Route
+    path="/register"
+    element={<Register/>}
+   />
+
+   <Route
+    path="/dashboard"
+    element={
+     <ProtectedRoute>
+      <Dashboard/>
+     </ProtectedRoute>
+    }
+   />
+
+  </Routes>
+
+ </BrowserRouter>
+ );
 }
 
 export default App;
