555bdf8f927b59a76fe5987c729fe535e7b3c505
Anshul Mankar <anshulmankar887@gmail.com>
2026-06-12 12:44:06 +0530

    Create App.tsx

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>SecureScan</h1>
+
+      <p>
+        Website & API Security Testing Platform
+      </p>
+    </div>
+  );
+}
+
+export default App;
